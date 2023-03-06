0d022e2992e2ec5bea5907674746236c3fd3d9d1
IhorKiy <kiyanichenko@ukr.net>
2023-03-06 17:32:35 +0200

    6.5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+
+const onPlay = function(time) {
+localStorage.setItem(STORAGE_KEY, time.seconds);
+};
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0,00001);
+player.on('timeupdate', throttle((onPlay), 1000));
+
+
+
+
