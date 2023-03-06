034d379ea894143639b89c2285574b2a0ba385ee
IhorKiy <kiyanichenko@ukr.net>
2023-03-06 13:23:36 +0200

    6.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const refElemGalery = document.querySelector(".gallery");
+const addCreateList = createList(galleryItems);
+refElemGalery.insertAdjacentHTML("beforeend", addCreateList);
+
+function createList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><div class="gallery">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></div></li>`;
+    })
+    .join("");
+}
+
+const instance = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+})
