1f3472d3c41aaf1e1e1ef7ab20169526315ebcdc
IhorKiy <kiyanichenko@ukr.net>
2023-03-07 20:54:37 +0200

    7.2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,32 +3,23 @@ import storage from './storage';
 
 const input = document.querySelector('.feedback-form');
 const emailValue = document.querySelector('input');
+const textlValue = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
 
 const arrayData = storage.load(STORAGE_KEY) || {};
 
-console.log(arrayData);
+if (arrayData) {
+  console.log(arrayData);
+  emailValue.value = arrayData.email;
+  textlValue.value = arrayData.message;
+}
 
 input.addEventListener('input', throttle(addLocalStor, 500));
-input.addEventListener('submit', throttle(handleSubmit,500));
-emailValue.addEventListener('input', throttle(updateOutput,500));
-
-
-updateOutput()
-
-function updateOutput() {
-  const savedData = storage.load(STORAGE_KEY);
-  if (savedData && savedData.email) {
-    emailValue.value = savedData.email;
-  }
-  if (savedData && savedData.message) {
-    input.elements.message.value = savedData.message;
-  }
-}
+input.addEventListener('submit', throttle(handleSubmit, 500));
 
 function addLocalStor(e) {
   e.preventDefault();
-   arrayData[e.target.name] = e.target.value;
+  arrayData[e.target.name] = e.target.value;
   storage.save(STORAGE_KEY, arrayData);
 }
 
@@ -41,6 +32,3 @@ function handleSubmit(e) {
   e.currentTarget.reset();
   storage.remove(STORAGE_KEY); // clear saved data from localStorage
 }
-
-
-
